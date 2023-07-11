eef95bc24a82b80bdd0b01ac92f6b5717671c852
phfeustel <philipp.feustel@voltstorage.com>
2023-07-11 16:26:29 +0200

    Draft PoC implementation: Integration support for Auth0 access tokens in OpenEMS backend

--- io.openems.backend.metadata.openidpostgres/src/io/openems/backend/metadata/openidpostgres/OpenIdPostgresMetadata.java
+++ io.openems.backend.metadata.openidpostgres/src/io/openems/backend/metadata/openidpostgres/OpenIdPostgresMetadata.java
@@ -121,28 +121,37 @@ public class OpenIdPostgresMetadata extends AbstractMetadata implements Metadata
     throw new OpenemsException("authenticate with username & password is not implemented");
   }
 
-  private Role lookupRole(String role) {
-    switch (role) {
-      case "admin":
-        return Role.ADMIN;
-      case "installer":
-        return Role.INSTALLER;
-      case "owner":
-        return Role.OWNER;
-      case "guest":
-        return Role.GUEST;
-      default:
-        return null;
-    }
+  private Role lookupRoleForKeycloakRole(String role) {
+    return switch (role) {
+      case "admin" -> Role.ADMIN;
+      case "installer" -> Role.INSTALLER;
+      case "owner" -> Role.OWNER;
+      case "guest" -> Role.GUEST;
+      default -> null;
+    };
+  }
+
+  private Role lookupRoleForAuth0(String role) {
+    return switch (role) {
+      case "admin" -> Role.ADMIN;
+      case "install" -> Role.INSTALLER;
+      case "owner" -> Role.OWNER;
+      case "guest" -> Role.GUEST;
+      default -> null;
+    };
   }
 
   public Map.Entry<Optional<Role>, NavigableMap<String, Role>> processJwt(String token) throws OpenemsNamedException {
     try {
       DecodedJWT jwt = JWT.decode(token);
 
+      String jwkProviderUrl = jwt.getIssuer().contains("auth0.com")
+          ? "https://dev-dripbmlnz2xh60k8.eu.auth0.com/.well-known/jwks.json"
+          : "http://localhost:8080/realms/voltstorage-customers/protocol/openid-connect/certs";
+
       // Get pub key for signature verification from JWK endpoint
       JwkProvider provider =
-          new UrlJwkProvider(new URL("http://localhost:8080/realms/voltstorage-customers/protocol/openid-connect/certs"));
+          new UrlJwkProvider(new URL(jwkProviderUrl));
       Jwk jwk = provider.get(jwt.getKeyId());
       Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
 
@@ -153,25 +162,47 @@ public class OpenIdPostgresMetadata extends AbstractMetadata implements Metadata
       verifier.verify(jwt);
 
       //Extract claims
-      List<String> groups = jwt.getClaim("groups").asList(String.class);
-      ArrayList<String> rolesClaim = (ArrayList<String>) (jwt.getClaim("realm_access").asMap()).get("roles");
-
-      Optional<Role> globalRole = rolesClaim.stream().map(this::lookupRole).filter(Objects::nonNull).sorted().findFirst();
-
-      NavigableMap<String, Role> roles = new TreeMap<>();
-      groups.stream().map(groupPath -> {
-        String[] groupHierarchy = groupPath.split("/");
-        String edgeId = groupHierarchy[1];
-        Role role = lookupRole(groupHierarchy[2].substring(0, groupHierarchy[2].length() - 1));
-        return new AbstractMap.SimpleEntry<>(edgeId, role);
-      }).forEach(entry -> roles.put(entry.getKey(), entry.getValue()));
-
-      return new AbstractMap.SimpleEntry<>(globalRole, roles);
+      return jwt.getIssuer().contains("auth0.com") ? mapAuth0Claims(jwt) : mapKeycloakClaims(jwt);
     } catch (TokenExpiredException exception) {
       throw OpenemsError.COMMON_AUTHENTICATION_FAILED.exception();
     } catch (JWTVerificationException | JwkException | MalformedURLException exception) {
       throw new RuntimeException(exception);
     }
+  }
+
+  private Map.Entry<Optional<Role>, NavigableMap<String, Role>> mapAuth0Claims(DecodedJWT jwt) {
+    List<String> permissions = jwt.getClaim("permissions").asList(String.class);
+
+    NavigableMap<String, Role> roles = new TreeMap<>();
+    permissions.forEach(permission -> {
+      String[] roleAtEdge = permission.split(":");
+      Role role = lookupRoleForAuth0(roleAtEdge[0]);
+      String edgeId = roleAtEdge[1];
+
+      if (role != null) {
+        roles.put(edgeId, role);
+      }
+    });
+
+    Optional<Role> globalRole = Optional.ofNullable(roles.firstEntry().getValue());
+    return new AbstractMap.SimpleEntry<>(globalRole, roles);
+  }
+
+  private AbstractMap.SimpleEntry<Optional<Role>, NavigableMap<String, Role>> mapKeycloakClaims(DecodedJWT jwt) {
+    List<String> groups = jwt.getClaim("groups").asList(String.class);
+    ArrayList<String> rolesClaim = (ArrayList<String>) (jwt.getClaim("realm_access").asMap()).get("roles");
+
+    Optional<Role> globalRole = rolesClaim.stream().map(this::lookupRoleForKeycloakRole).filter(Objects::nonNull).sorted().findFirst();
+
+    NavigableMap<String, Role> roles = new TreeMap<>();
+    groups.stream().map(groupPath -> {
+      String[] groupHierarchy = groupPath.split("/");
+      String edgeId = groupHierarchy[1];
+      Role role = lookupRoleForKeycloakRole(groupHierarchy[2].substring(0, groupHierarchy[2].length() - 1));
+      return new AbstractMap.SimpleEntry<>(edgeId, role);
+    }).forEach(entry -> roles.put(entry.getKey(), entry.getValue()));
+
+    return new AbstractMap.SimpleEntry<>(globalRole, roles);
   }
 
   @Override
